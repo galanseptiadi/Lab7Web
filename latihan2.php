76331f3d9dd5c723a558f7ed49fc0e635e1c23e9
galanseptiadi <101730390+galanseptiadi@users.noreply.github.com>
2022-05-18 22:57:35 +0700

    Add files via upload

---
+++ latihan2.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title> PHP Dasar</title>
+</head>
+<body>
+    <h1>Belajar PHP Dasar</h1>
+    <?php
+    echo "Hello World";
+    ?>
+    
+    <h2>Menggunakan Variable</h2>
+    <?php
+    $nim = "312010053";
+    $nama = 'Galan';
+    echo "NIM : " . $nim . "<br>" ;
+    echo "nama : $nama";
+    ?>
+
+    <h3>Predefine Variable $_GET</h3>
+    <?php
+    echo 'Selamat Datang ' . $_GET['nama'];
+    ?>
+
+</body>
+</html>
+
